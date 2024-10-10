19001c0c17723fc7f3bd90006198b39e249997c7
Tanishk <tanishkraj70@gmail.com>
2024-10-10 12:43:41 +0530

    Work done by TeamB

---
+++ PracticalGit/TeamB.py
@@ -0,0 +1,13 @@
+import math
+
+def square_root(a):
+    return math.sqrt(a)
+
+def cube(a):
+    return a ** 3
+
+def area_rectangle(length, width):
+    return length * width
+
+def area_circle(radius):
+    return math.pi * radius ** 2
